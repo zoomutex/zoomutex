49d79525883a272fd80431b221f8f514da076662
dalyIsaac <isaac.daly@outlook.com>
2021-04-04 20:23:00 +1200

    Fixed socket URL

--- client/src/context/SocketContext.ts
+++ client/src/context/SocketContext.ts
@@ -1,5 +1,5 @@
 import React from "react";
 import { io } from "socket.io-client";
 
-export const socket = io(`http://localhost:3000`);
+export const socket = io(window.location.origin);
 export const SocketContext = React.createContext(socket);
