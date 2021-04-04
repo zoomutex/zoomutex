f921eada3bd62116d2152b880e77e18b550b0d42
dalyIsaac <isaac.daly@outlook.com>
2021-04-04 17:39:11 +1200

    Server can now serve the client

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import { createServer } from "http";
 import express from "express";
 import helmet from "helmet";
 import initSockets from "./sockets";
+import path from "path";
 
 dotenv.config();
 
@@ -20,6 +21,22 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+const parent = (p: string): string => {
+  return path.dirname(p);
+};
+
+if (process.env.NODE_ENV === "production") {
+  const clientPath = parent(parent(__dirname));
+  const buildPath = path.join(clientPath, "client", "build");
+  const indexPath = path.join(buildPath, "index.html");
+
+  app.use(express.static(buildPath));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(indexPath);
+  });
+}
 
 // HTTP server
 const httpServer = createServer(app);
