2a7c503ab456f26a2177d0d8c4f560beedbe4ade
Ishan Goyal <ishan.goyal@student.unimelb.edu.au>
2021-04-08 21:35:26 +1000

    predeploy script to build rootDir/, push this directory to deployment-test branch to trigger deployment

---
+++ predeploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#act as a new root folder
+rm -rf rootDir
+mkdir rootDir/
+
+cd client
+rm -rf build 
+npm run build
+mv build clientBuild
+cp -r clientBuild ../
+rm -rf clientBuild
+cd ..
+
+cp -r clientBuild rootDir/
+cp -r server/* rootDir/
+
+rm -rf clientBuild/
+
+
+
+
+
+
+
+
+ 
+
